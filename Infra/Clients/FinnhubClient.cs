7b263fbbd342f3289265a956d4d0154d41e548bc
Lawand Rasoul <lawand.rasoul@qinshift.com>
2025-02-10 11:38:21 +0100

    Add project files.

---
+++ Infra/Clients/FinnhubClient.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StockDataApp.Infra.Clients
+{
+    public class FinnhubClient
+    {
+        private readonly HttpClient httpClient;
+
+        public FinnhubClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<string> GetStockSymbolByQueryAsync(string query)
+        {
+            var response = await httpClient.GetAsync($"api/v1/search?q={query}");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using StockDataApp.Components;
+using StockDataApp.Infra.Clients;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<FinnhubClient>(c =>
+{
+    c.DefaultRequestHeaders.Add("X-Finnhub-Token", builder.Configuration.GetValue<string>("FinnhubApiKey"));
+    c.BaseAddress = new Uri("https://finnhub.io/");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
