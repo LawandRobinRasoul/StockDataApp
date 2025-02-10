0f910d5635ada55834af83531ccd8ad7c4e744aa
Lawand Rasoul <lawand.rasoul@qinshift.com>
2025-02-10 12:09:02 +0100

    Adding contract to finnhub interface

--- Core/IFinnhubService.cs
+++ Core/IFinnhubService.cs
@@ -3,5 +3,17 @@
     public interface IFinnhubService
     {
         Task<string> GetStockSymbolByQueryAsync(string query);
+
+        Task<string> GetStockPriceToday(string symbol);
+
+        Task<string> GetBasicFinancialsAsync(string symbol);
+
+        Task<string> GetPeersAsync(string symbol);
+
+        Task<string> GetCompanyProfileAsync(string symbol);
+
+        Task<string> GetRecommendationsAsync(string symbol);
+
+        Task<string> GetFinancialsAsync(string symbol, string frequency, FromKeyedServicesAttribute, DateTimeOffset from, DateTimeOffset to);
     }
 }
